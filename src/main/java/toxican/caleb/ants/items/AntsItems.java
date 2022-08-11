36caa716c6c7436674f438ceb3f7074a4c6752d4
MastroCaleb <zypper4509@gmail.com>
2022-08-11 23:18:10 +0200

    muddy spawnegg

--- src/main/java/toxican/caleb/ants/items/AntsItems.java
+++ src/main/java/toxican/caleb/ants/items/AntsItems.java
@@ -27,6 +27,7 @@ public class AntsItems {
     public static final SpawnEggItem BLACK_ANT_SPAWN_EGG = new SpawnEggItem(AntsEntities.BLACK_ANT, 0x3b332c, 0x2c2c24, new Item.Settings().maxCount(1).group(ItemGroup.MISC));
     public static final SpawnEggItem RED_ANT_SPAWN_EGG = new SpawnEggItem(AntsEntities.RED_ANT, 0xcc543c, 0xa43c44, new Item.Settings().maxCount(1).group(ItemGroup.MISC));
     public static final SpawnEggItem GOLD_ANT_SPAWN_EGG = new SpawnEggItem(AntsEntities.GOLD_ANT, 0xfcb424, 0xfca404, new Item.Settings().maxCount(1).group(ItemGroup.MISC));
+    public static final SpawnEggItem MUDDY_ANT_SPAWN_EGG = new SpawnEggItem(AntsEntities.MUDDY_ANT, 0x3b2717, 0x745434, new Item.Settings().maxCount(1).group(ItemGroup.MISC));
 
     public static final ClayBottleItem CLAY_BOTTLE = new ClayBottleItem(new Item.Settings().maxCount(16).group(ItemGroup.FOOD).food(ClayBottleFoodComponent.CLAY_BOTTLE_COMPONENT).recipeRemainder(Items.GLASS_BOTTLE));
 
@@ -41,6 +42,7 @@ public class AntsItems {
         Registry.register(Registry.ITEM, new Identifier(AntsMain.MOD_ID, "red_ant_egg"), RED_ANT_SPAWN_EGG);
         Registry.register(Registry.ITEM, new Identifier(AntsMain.MOD_ID, "black_ant_egg"), BLACK_ANT_SPAWN_EGG);
         Registry.register(Registry.ITEM, new Identifier(AntsMain.MOD_ID, "gold_ant_egg"), GOLD_ANT_SPAWN_EGG);
+        Registry.register(Registry.ITEM, new Identifier(AntsMain.MOD_ID, "muddy_ant_egg"), MUDDY_ANT_SPAWN_EGG);
 
         Registry.register(Registry.ITEM, new Identifier(AntsMain.MOD_ID, "clay_bottle"), CLAY_BOTTLE);
     }

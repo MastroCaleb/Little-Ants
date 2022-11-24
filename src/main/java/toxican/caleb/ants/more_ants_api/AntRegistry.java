9177a34cd5541afc48f98d0fcdc30f3a149543d3
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-11-24 22:42:39 +0100

    ant variants save their registry key

--- src/main/java/toxican/caleb/ants/more_ants_api/AntRegistry.java
+++ src/main/java/toxican/caleb/ants/more_ants_api/AntRegistry.java
@@ -1,6 +1,7 @@
 package toxican.caleb.ants.more_ants_api;
 
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.DefaultedRegistry;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import toxican.caleb.ants.AntsMain;
@@ -8,9 +9,10 @@ import toxican.caleb.ants.AntsMain;
 public class AntRegistry{
 
     public static final RegistryKey<Registry<AntVariant>> ANT_VARIANT_KEY = AntRegistry.createRegistryKey("ant_variant");
-    public static final Registry<AntVariant> ANT_VARIANT = Registry.create(ANT_VARIANT_KEY, registry -> AntVariant.BROWN);
+    public static final DefaultedRegistry<AntVariant> ANT_VARIANT = Registry.create(ANT_VARIANT_KEY, "brown", AntVariant::getRegistryEntry, registry -> AntVariant.BROWN);
     
     private static <T> RegistryKey<Registry<T>> createRegistryKey(String registryId) {
         return RegistryKey.ofRegistry(new Identifier(AntsMain.MOD_ID, registryId));
     }
+    
 }
